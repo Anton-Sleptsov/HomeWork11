cdb6e45de5051cb43cef455141e374745255d31d
Anton-Sleptsov <arslepcov@kait20.ru>
2024-04-21 22:18:44 +0300

    Добавьте файлы проекта.

---
+++ HomeWork11/ComparablePair.cs
@@ -0,0 +1,23 @@
+﻿namespace HomeWork11
+{
+    internal class ComparablePair<T, U> : IComparable<ComparablePair<T, U>> 
+        where T : IComparable<T> 
+        where U : IComparable<U>
+    {
+        public T t {  get; }
+        public U u { get; }
+
+        public ComparablePair(T t, U u)
+        {
+            this.t = t;
+            this.u = u;
+        }
+
+        public int CompareTo(ComparablePair<T, U>? other)
+        {
+            int tCompare = t.CompareTo(other.t);
+            int uCompare = u.CompareTo(other.u);
+            return tCompare == 0 ? uCompare : tCompare;
+        }
+    }
+}
---
+++ HomeWork11/Pair.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeWork11
+{
+    internal class Pair<S, T> (S firstValue, T secondValue)
+    {
+        public S FirstValue { get; } = firstValue;
+        public T SecondValue { get; } = secondValue;
+
+    }
+}
